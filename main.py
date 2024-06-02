a0b0783b7046705924873db0c9881020f959a0d2
yousef12102008 <moh552vbnm@gmail.com>
2024-06-02 04:29:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ def main(message):
                     bot.reply_to(message, msg)
                 else:
                     dd += 1
-                time.sleep(21)
+                time.sleep(5)
     except Exception as e:
         print(e)
     bot.edit_message_text(chat_id=message.chat.id, message_id=ko, text='𝗕𝗘𝗘𝗡 𝗖𝗢𝗠𝗣𝗟𝗘𝗧𝗘𝗗 ✅\n𝗕𝗢𝗧 𝗕𝗬 ➜ @Af5AA')
