5f6858dd0784cedfe90e3d678b417a59e0b26516
yousef12102008 <moh552vbnm@gmail.com>
2024-06-02 05:08:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
-import telebot
-import time
+import telebot, time
 from telebot import types
 from mk import Tele
 import os
@@ -8,32 +7,32 @@ import os
 token = '6848019028:AAGDVZ4MIlMKOL0pRjtjMOadz4qkf9cqarU'
 bot = telebot.TeleBot(token, parse_mode="HTML")
 
-# قائمة ID المسموح لهم
-allowed_ids = [6309252183, 6505725294, 6429416876, 5964228363, 1072224102, 6522495478, 5196018289, 5746463671, 5491877666, 5887064826, 2123721043, 1104251796, 5202679708, 6298754267]
+# List of authorized user IDs
+authorized_users = [
+    '6309252183', '6505725294', '6429416876', '5964228363', '1072224102', 
+    '6522495478', '5196018289', '5746463671', '5491877666', '5887064826', 
+    '2123721043', '1104251796', '5202679708', '6298754267'
+]
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    if message.chat.id not in allowed_ids:
+    if str(message.chat.id) not in authorized_users:
         bot.reply_to(message, "You cannot use the bot. Contact developers to purchase a bot subscription @Af5AA")
         return
-    bot.reply_to(message, "Send the file now \n ارسل الملف الان")
-
-
+    bot.reply_to(message, "جو الهكر يرحب بكم\nSend the file now \n ارسل الملف الان")
 
 @bot.message_handler(content_types=["document"])
 def main(message):
-    if message.chat.id not in allowed_ids:
+    if str(message.chat.id) not in authorized_users:
         bot.reply_to(message, "You cannot use the bot. Contact developers to purchase a bot subscription @Af5AA")
         return
-
     dd = 0
     live = 0
     ch = 0
-    ko = (bot.reply_to(message, "Checking Your Cards...⌛").message_id)
+    ko = bot.reply_to(message, "Checking Your Cards...⌛").message_id
     ee = bot.download_file(bot.get_file(message.document.file_id).file_path)
     with open("combo.txt", "wb") as w:
         w.write(ee)
-
     try:
         with open("combo.txt", 'r') as file:
             lino = file.readlines()
@@ -50,30 +49,29 @@ def main(message):
                 except:
                     pass
                 try:
-                    bank = data['bank']['name']
+                    bank = (data['bank']['name'])
                 except:
-                    bank = '𝒖𝒏𝒌𝒏𝒐𝒘𝒏'
+                    bank = ('𝒖𝒏𝒌𝒏𝒐𝒘𝒏')
                 try:
-                    emj = data['country']['emoji']
+                    emj = (data['country']['emoji'])
                 except:
-                    emj = '𝒖𝒏𝒌𝒏𝒐𝒘𝒏'
+                    emj = ('𝒖𝒏𝒌𝒏𝒐𝒘𝒏')
                 try:
-                    cn = data['country']['name']
+                    cn = (data['country']['name'])
                 except:
-                    cn = '𝒖𝒏𝒌𝒏𝒐𝒘𝒏'
+                    cn = ('𝒖𝒏𝒌𝒏𝒐𝒘𝒏')
                 try:
-                    dicr = data['scheme']
+                    dicr = (data['scheme'])
                 except:
-                    dicr = '𝒖𝒏𝒌𝒏𝒐𝒘𝒏'
+                    dicr = ('𝒖𝒏𝒌𝒏𝒐𝒘𝒏')
                 try:
-                    typ = data['type']
+                    typ = (data['type'])
                 except:
-                    typ = '𝒖𝒏𝒌𝒏𝒐𝒘𝒏'
+                    typ = ('𝒖𝒏𝒌𝒏𝒐𝒘𝒏')
                 try:
-                    url = data['bank']['url']
+                    url = (data['bank']['url'])
                 except:
-                    url = '𝒖𝒏𝒌𝒏𝒐𝒘𝒏'
-
+                    url = ('𝒖𝒏𝒌𝒏𝒐𝒘𝒏')
                 try:
                     last = str(Tele(cc))
                 except Exception as e:
@@ -93,30 +91,29 @@ def main(message):
                 mes.add(cm1, status, cm3, cm4, cm5, stop)
                 bot.edit_message_text(chat_id=message.chat.id, message_id=ko, text='''Wait for processing 
 𝒃𝒚 ➜ @Af5AA ''', reply_markup=mes)
-                msg = f'''◆ 𝑪𝑨𝑹𝑫  ➜ {cc} 
-◆ 𝑺𝑻𝑨𝑻𝑼𝑺 ➜ 𝗔𝗽𝗽𝗿𝗼𝘃𝗲𝗱  ✅ 
-◆ 𝑹𝑬𝑺𝑼𝑳𝑻 ➜ #Approved
-◆ 𝑮𝑨𝑻𝑬𝑾𝑨𝒀 ➜ 𝙱𝚁𝙰𝙸𝙽𝚃𝚁𝙴𝙴 𝙰𝚄𝚃𝙷 
+                msg = f''' 𝐂𝐚𝐫𝐝 ➜ {cc} 
+_   𝐀𝐩𝐩𝐫𝐨𝐯𝐞𝐝 ✅
+◆ 𝐆𝐚𝐭𝐞𝐰𝐚𝐲 ➜ Braintree Auth
 ━━━━━━━━━━━━━━━━━
-◆ 𝑩𝑰𝑵 ➜ {cc[:6]} - {dicr} - {typ} 
-◆ 𝑪𝑶𝑼𝑵𝑻𝑹𝒀 ➜ {cn} - {emj} 
-◆ 𝑩𝑨𝑵𝑲 ➜ {bank}
-◆ 𝑼𝑹𝑳 ➜ {url}
-━━━━━━━━━━━━━━━━━
+◆ 𝐁𝐈𝐍 ➜ {cc[:6]} - {dicr} - {typ} 
+◆ 𝐂𝐨𝐮𝐧𝐭𝐫𝐲 ➜ {cn} - {emj} 
+◆ 𝐁𝐚𝐧𝐤 ➜ {bank} 
 ◆ 𝑩𝒀: @Af5AA
-◆𝑷𝑹𝑶𝑿𝒀𝑺: 𝑷𝑹𝑶𝒙𝒀 𝑳𝑰𝑽𝑬 ✅ '''
                 print(last)
                 if "live" in last or 'Approved' in last:
                     live += 1
                     bot.reply_to(message, msg)
                 else:
                     dd += 1
-                time.sleep(5)
+                time.sleep(21)
     except Exception as e:
         print(e)
     bot.edit_message_text(chat_id=message.chat.id, message_id=ko, text='𝗕𝗘𝗘𝗡 𝗖𝗢𝗠𝗣𝗟𝗘𝗧𝗘𝗗 ✅\n𝗕𝗢𝗧 𝗕𝗬 ➜ @Af5AA')
 
-
+@bot.callback_query_handler(func=lambda call: call.data == 'stop')
+def menu_callback(call):
+    with open("stop.stop", "w") as file:
+        pass
 
 print("تم تشغيل البوت")
-bot.infinity_polling()
+bot.polling()
